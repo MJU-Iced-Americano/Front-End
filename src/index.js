9f2af6b24e08bfc9802b52fcaa3dbcdaea9c8cc7
221B0825 <eunseoyu0825@gmail.com>
2023-03-31 23:43:46 +0900

    first popup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StaticExample from "./modal/StaticExample";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <StaticExample></StaticExample>
   </React.StrictMode>
 );
 
