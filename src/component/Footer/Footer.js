45cd2e6c119318bd563e2c3b6402e3628159d90a
Hanaux <hsh990821@gmail.com>
2023-04-03 14:04:43 +0900

    MainPage Task 01

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,26 @@
+import './footer.css';
+import SocoaF from '../socoa-ver2.png';
+
+const Footer =() => {
+    return (
+        <footer className="footer">
+            <div className="footerAreaLeft">
+                <div className="logoBG"><img src={SocoaF} alt='logo image' className="socoaLogoF"/></div>
+                <div className="textField">
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                </div>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
