82b1b38b31c6a55037bd038d5e31c0babaa27658
Hanaux <hsh990821@gmail.com>
2023-06-06 01:03:10 +0900

    <06.06> (1) Footer 내용 및 폰트, Height 수정 (2) Mainpage 바운더리 수정 (3) Header 섀도우 추가

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,17 +5,20 @@ const Footer =() => {
     return (
         <footer className="footer">
             <div className="footerAreaLeft">
-                <div className="logoBG"><img src={SocoaF} alt='logo image' className="socoaLogoF"/></div>
+                <div className="logoBG">
+                    <img src={SocoaF} alt='logo image' className="socoaLogoF"/>
+                </div>
+                <div className="logoUnder">
+                    <p>개인정보처리방침 | 이용약관 </p>
+                </div>
                 <div className="textField">
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
-                    왜 안될까요? 모르겠습니다 다시 한번 해보세요
+                    (주)제이유컨설팅 | 대표자: 정진성 | 사업자번호: 314-86-24768
+                </div>
+                <div className="textField">
+                    주소: 경기도 성남시 분당구 불정로 397 308-1102
+                </div>
+                <div className="textField uphere">
+                    ©JUCONSULTING. ALL RIGHTS RESERVED
                 </div>
 
             </div>
